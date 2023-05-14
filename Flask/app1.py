a7343d412fdf287035a57bb2fe0f518b6b4533ba
TSxSAHIL <tssahil2001@gmail.com>
2023-05-14 15:48:19 +0530

    postman

---
+++ Flask/app1.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+import pickle
+from flask_ngrok import run_with_ngrok
+import numpy as np
+import pandas as pd
+
+diabetes = pickle.load(open("Flask\diabetes_model.pkl",'rb'))
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+@app.route('/mrs' , methods=['POST'])
+def diabetes_prediction():
+    # Get the input data from the request
+    input_data = request.get_json()
+
+    # Extract the input values from the data
+    pregnancies = int(input_data['pregnancies'])
+    glucose = int(input_data['glucose'])
+    blood_pressure = int(input_data['blood_pressure'])
+    skin_thickness = int(input_data['skin_thickness'])
+    insulin = int(input_data['insulin'])
+    bmi = float(input_data['bmi'])
+    diabetes_pedigree_function = float(input_data['diabetes_pedigree_function'])
+    age = int(input_data['age'])
+
+    # Create a numpy array with the input values
+    input_data = np.array([[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree_function, age]])
+
+    # Reshape the input data to match the expected shape of the model
+    input_data_reshaped = input_data.reshape(1,-1)
+
+    # Make the prediction using the loaded model
+    prediction = diabetes.predict(input_data_reshaped)
+
+    # Return the predicted outcome as a JSON response
+    if (prediction[0] == 0):
+        return jsonify({'prediction': 'Diabetes Negative'})
+    else:
+        return jsonify({'prediction': 'Diabetes Positive'})
+    
+if __name__ == '__main__':
+    app.run()
---
+++ Model/api_implementation.py
@@ -0,0 +1,23 @@
+import json
+import requests
+
+url = 'http://127.0.0.1:8000/diabetes_prediction'
+
+input_data_model = {
+
+    'Pregnancies' : 0,
+    'Glucose': 148,
+    'BloodPressure': 72,
+    'SkinThickness' : 35,
+    'Insulin' : 0,
+    'BMI' : 33.6,
+    'DiabetesPedigreeFunction' : 0.627,
+    'Age' : 60
+
+}
+
+input_json = json.dumps(input_data_model)
+
+response = requests.post(url, data=input_json)
+
+print(response.text)
